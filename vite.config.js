15f103492a2610ddf46e974011e20cf698e01bef
Ben Tea <ben@shippabo.com>
2023-02-16 17:04:35 -0800

    bump vite version to latest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [react(), basicSsl()],
+  server: {
+    https: true,
+  },
 })
